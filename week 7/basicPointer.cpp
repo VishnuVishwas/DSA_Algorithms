3c6476d33a54af3af33a5e908c9825d18f3f0fc3
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-03-04 11:00:37 +0530

    Pointers

--- week 7/basicPointer.cpp
+++ week 7/basicPointer.cpp
@@ -9,4 +9,44 @@ int main() {
     cout << &a << endl;
     cout << ptr << endl;
     cout << *ptr << endl;
+
+    // size of different data type pointers
+    int b = 10;
+    int* p = &b;
+    cout << "sizeof(p) : " << sizeof(p) << endl;
+
+    float c = 10;
+    float* q = &c;
+    cout << "sizeof(q) : " << sizeof(q) << endl;
+
+    char d = '10';
+    char* r = &d;
+    cout << "sizeof(r) : " << sizeof(r) << endl;
+
+    double e = 1.03;
+    double* s = &e;
+    cout << "sizeof(s) : " << sizeof(s) << endl;
+
+    // pointer declaration 
+    int *t;
+    cout << t << endl;  // prints garbage value 
+    cout << *t << endl; // segmentation error or does not print anything
+
+    // Null pointer declaration
+    int *u = 0;
+    cout << u << endl;
+    cout << *u << endl;
+
+    // Null pointer declaration - 2
+    int *v = nullptr;
+    cout << v << endl;
+    cout << *v << endl;
+
+    // copy pointer
+    int f = 43;
+    int* w = &f;
+    int* x = w;
+
+    cout << w <<endl;     // Both 'w'and 'x' will be poiting to address of 'f'
+    cout << x << endl;
 }
