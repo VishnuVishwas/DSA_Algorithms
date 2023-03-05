1a86e9dd2f53cbf6bdf5e547ac5801fee388d3f4
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-03-05 11:09:24 +0530

    Type casting
    Implicit
    Explicit
    Over flow

---
+++ week 1/typeCasting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Implicit type casting
+    char ch = 97;
+    cout <<" Implicit type cast : " << ch << endl;
+
+    // Explicit type cast
+    char ch1 = char(98);
+    cout <<"Explicit type cast : " << ch1 << endl;
+
+    // overflow
+    char ch2 = 999;    
+    cout << ch2 << endl;  // a unexpected value is printed
+
+    cout << true + false;
+}
