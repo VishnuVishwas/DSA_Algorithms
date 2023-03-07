ffbe87921d39eee4fb0c2eb97a65068eee5a4563
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-03-07 21:11:03 +0530

    Occurence of element
    1. First occurence
    2. Last occurence

---
+++ week 4/3_Occ.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int firstOcc (int arr[], int size, int key) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+
+    while (start <= end) {
+        if (key == arr[mid]) {
+            ans = mid;
+            end = mid - 1;
+        }
+
+        else if (key < arr[mid] ) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start)/2;
+    }
+    cout << "\nThe element present at : " << ans;
+}
+
+int lastOcc (int arr[], int size, int key) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+
+    while (start <= end) {
+        if (key == arr[mid]) {
+            ans = mid;
+            start = mid + 1;
+        }
+
+        else if (key < arr[mid] ) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start)/2;
+    }
+    cout << "\nThe element present at : " << ans;
+}
+
+int main() {
+    int arr[1990] = {3, 4, 8, 8, 8, 8 ,8, 23, 34, 45};
+    int key;
+
+    cout << "Enter the key : ";
+    cin >> key;
+
+    cout << "\nElements are : ";
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+
+    int size;
+
+    firstOcc(arr, size = 10, key);
+    lastOcc(arr, size = 10, key);
+
+    return 0;
+}
