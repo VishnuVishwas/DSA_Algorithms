c7251bcf60d2e15e33288692c1962d44960b70ca
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-03-11 21:21:42 +0530

    Factorial

--- week 7/1_Factorial.cpp
+++ week 7/1_Factorial.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n) {
-    if (n == 0) {
-        return 0;
-    }
-
-    if (n == 1) {
+int fact(int n) {
+    if (n == 0 || n == 1) {
         return 1;
     }
 
-    return n * factorial(n-1);
+    return n * fact(n-1);
 }
 
 int main() {
     int n;
-    cout << "Enter the value of n : ";
+
+    cout << "Enter the value of n : " ;
     cin >> n;
 
-    int number = factorial(n);    
-    cout << number;
+    int value = fact(n);
+    cout << "The factorial of number is : " << value;
 }
