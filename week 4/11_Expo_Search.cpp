730f1787ea7738bc9e5bb3952d1a0db914061958
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-06-24 12:55:47 +0530

    Exponential Search for Binary Search

--- week 4/11_Expo_Search.cpp
+++ week 4/11_Expo_Search.cpp
@@ -1,8 +1,51 @@
 // Exponential Search for Binary Search
-// Note that we know the value of n in this array : i.e for bound index
+// considering the window size of elements and applying binarySearch
 
 #include<iostream>
 using namespace std;
+
+int binarySearch(int arr[], int i, int j, int x);
+
+// Exponential Search when we know the size of array
+int expoSearch(int arr[], int n, int x) {
+    if(arr[0] == x) {
+        return 0;
+    }
+
+    int i=1;
+    while(i<n && arr[i]<=x) {
+        i = i * 2;
+    }
+
+    return binarySearch(arr, i/2, min(i, n-1), x);
+}
+
+// unbound -> when there are infinite array elements and the size is unknown
+int expoUnbound(int arr[], int n, int x) {
+    if(arr[0] == x) {
+        return 0;
+    }
+
+    int i=0;
+    int j=1;
+
+    while(j<n && arr[j]<=x) {
+        i = j;
+        j = j * 2;
+    }
+
+    return binarySearch(arr, j/2, min(j, n-1), x);
+}
+
+int main() {
+    int arr[] = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int x = 13;
+
+    int index = expoUnbound(arr, n, x);
+    cout << "The index of " << x << " is " << index;
+}
 
 int binarySearch(int arr[], int i, int j, int x) {
     while (i<=j) {
@@ -20,27 +63,4 @@ int binarySearch(int arr[], int i, int j, int x) {
     }
 
     return -1;
-}
-
-int expoSearch(int arr[], int n, int x) {
-    if(arr[0] == x) {
-        return 0;
-    }
-
-    int i=1;
-    while(i<n && arr[i]<=x) {
-        i = i * 2;
-    }
-
-    return binarySearch(arr, i/2, min(i, n-1), x);
-}
-
-int main() {
-    int arr[] = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    int x = 13;
-
-    int index = expoSearch(arr, n, x);
-    cout << "The index of " << x << " is " << index;
 }
