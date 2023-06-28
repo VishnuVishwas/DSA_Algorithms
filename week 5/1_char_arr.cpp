b76ff58c0f71d8b38024de8bcf5e51f12690de0c
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-06-28 22:09:23 +0530

    Character String Basics

---
+++ week 5/1_char_arr.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // char arr[100];                            // the input can be taken directly using cout 
+    // cout << "Enter the string : ";            // '\0' character will be added by default indicating end of string 
+    // cin >> arr;
+    // cout << arr;
+
+    // char arr[5];
+    // arr[0] = 'A';
+    // arr[1] = 'B';
+    // cout << "Enter the element for 3rd poisition : ";    // we can take characters index by index
+    // cin >> arr[3];
+    // cout << arr[0] << " " << arr[1] << " " << arr[3];
+
+    // char arr[100] = "Vishnu Vishwas";
+    // cout << arr;
+
+    // char arr[100];
+    // cout << "Enter your name : ";
+    // cin >> arr;
+    // cout << "Name : " << arr;  // only upto space printed 
+
+    // char arr[100];
+    // cin.getline(arr, 50);      // to take input with spaces
+    // cout << arr;
+
+    // char arr[6] = "Vishnu";       // Throws error because we a need a space for '\0' character
+    
+}
