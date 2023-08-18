8fcb462a3ce93a022aeb49c88773619f47a858c2
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-08-18 19:58:00 +0530

    Add strings

--- week 7/16_add_two_strings.cpp
+++ week 7/16_add_two_strings.cpp
@@ -1,32 +1,39 @@
-class Solution {
-public:
+// add 2 strings 
+// you must not convert the inputs to integers directly
 
-    void addRE(string& num1, int p1, string& num2, int p2, int carry, string& ans) {
+#include<iostream>
+#include<algorithm>
+using namespace std;
 
-        if(p1<0 && p2<0) {
-            if(carry != 0) {
-                ans.push_back(carry + '0');
-            }
+void addStrings(string& num1, int i, string& num2, int j, string& ans, int carry) {
 
-            return;
+    if(i<0 && j<0) {
+        if(carry != 0) {
+            ans.push_back(carry + '0');
         }
-
-        int n1 = (p1>=0 ? num1[p1] : '0') - '0';
-        int n2 = (p2>=0 ? num2[p2] : '0') - '0';
-
-        int totalSum = n1+n2+carry;
-        int digit = totalSum%10;
-        carry = totalSum/10;
-        ans.push_back(digit + '0');
-
-        addRE(num1, p1-1, num2, p2-1, carry, ans);
+        return;
     }
 
-    string addStrings(string num1, string num2) {
-        string ans = "";
-        addRE(num1, num1.size()-1, num2, num2.size()-1, 0, ans);
-        reverse(ans.begin(), ans.end());
+    int n1 = (i >= 0 ? (num1[i]) : '0') - '0';
+    int n2 = (j >= 0 ? (num2[j]) : '0') - '0';
+    
+    int totalSum = n1 + n2 + carry;
 
-        return ans;
-    }
-}; 
+    int digit = totalSum%10;
+    carry = totalSum/10;
+    ans.push_back(digit + '0');
+
+    addStrings(num1, i-1, num2, j-1, ans, carry);
+}
+
+int main() {
+    string num1 = "456";
+    string num2 = "77";
+
+    string ans = "";
+
+    addStrings(num1, num1.size()-1, num2, num2.size()-1, ans, 0);
+
+    reverse(ans.begin(), ans.end());
+    cout << "The of the two strings is : " << ans << endl;
+}
