0f6588404602d863b5f9340085a72d6758b998d2
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-09-11 15:48:16 +0530

    Upcasting

--- week 9/10_upcasting.cpp
+++ week 9/10_upcasting.cpp
@@ -1,22 +1,23 @@
+// first part of the object print hoga
+
 #include<iostream>
 using namespace std;
 
 class Animal {
     public:
-        void speak() {
+        virtual void speak() {                        // upcasting 
             cout << "Speak" << endl;
         }
 };
 
 class Dog : public Animal{
     public:
-        virtual void speak() {
+        void speak() {
             cout << "Barking" << endl;
         }
 };
 
 int main() {
     Animal* obj = new Dog();
-
-
+    obj->speak();                      // since we upcasted, output will be "Barking"
 }
