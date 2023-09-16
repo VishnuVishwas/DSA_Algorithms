bafce4b08801d3bbf4c1e088c4672f6efb3683d3
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-09-16 22:28:25 +0530

    Middle Node of LInked List

---
+++ week 10 Linked List/5_Middle_Node.cpp
@@ -0,0 +1,133 @@
+// find the middle Node of LL 
+
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+int getLen(Node* head) {
+    Node* temp = head;
+    int length = 0;
+
+    while(temp != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    return length;
+}
+
+void insertAtHead(Node* &head, Node* &tail, int data) {
+    if(head == NULL) {
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else{
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    }
+}
+
+void insertAtTail(Node* &head, Node* &tail, int data) {
+    if(head == NULL) {
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    else{
+        Node* newNode = new Node(data);
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void insertAtPos(Node* &head, Node* &tail, int data, int pos) {
+    if(head == NULL) {
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    
+    if(pos == 0) {
+        insertAtHead(head, tail, data);
+        return;
+    }
+    
+    int len = getLen(head);
+    if(pos == len) {
+        insertAtTail(head, tail, data);
+        return;
+    }
+
+    Node* prev = head;
+    int i=1;
+    while(i < pos){
+        prev = prev->next;
+        i++;
+    }
+
+    Node* curr = prev->next;
+    Node* newNode = new Node(data);
+
+    newNode->next = curr;
+    prev -> next = newNode;
+    
+}
+
+void print(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node* MiddleNode(Node* &head) {
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != NULL) {
+        fast = fast->next;
+        if(fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+
+    return slow;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    // cout << "Inserting at head : ";
+    insertAtHead(head, tail, 5);
+    insertAtHead(head, tail, 15);
+    insertAtHead(head, tail, 52);
+    insertAtHead(head, tail, 5);
+    insertAtHead(head, tail, 55);
+    insertAtHead(head, tail, 75);
+    // print(head);
+
+    Node* middle = MiddleNode(head);
+    cout << "Middle Linked List of data : " << middle->data << endl;
+}
---
+++ week 10 Linked List/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    // cout << "Inserting at head : ";
