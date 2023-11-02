59ab5a8c0fa49bd79a780f8876fbd2cc0b7b4331
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-11-02 19:02:04 +0530

    User defined Queue

---
+++ week 11 Queue/tempCodeRunnerFile.cpp
@@ -0,0 +1,77 @@
+// User defined Queue 
+
+#include<iostream>
+using namespace std;
+
+class Queue{
+public:
+    int* arr;
+    int size;
+    int front;
+    int rear;
+
+    Queue(int size) {
+        this->size = size;
+        arr = new int[size];
+        front = 0;
+        rear = 0;
+    }    
+
+    void push(int data) {
+        if(rear == size) {
+            cout << "Queue is full " << endl;
+        }
+        else {
+            arr[front] = data;
+            rear++;
+        }
+    }
+
+    void pop() {
+        if(front == rear) {
+            cout << "Queue is full " << endl;
+        }
+        else{
+            arr[front] = -1;
+            front++;
+            if(front == rear) {
+                front = 0;
+                rear = 0;
+            }
+        }
+    }
+
+    int getFront() {
+        if(front == rear) {
+            cout << "Queue is empty " << endl;
+            return -1;
+        }
+        else 
+            return arr[front];
+    }
+
+    int size() {
+        return rear-front;
+    }
+
+    bool isEmpty() {
+        if(front == rear) 
+            return true;
+        else 
+            return false;
+    }
+
+
+};
+
+int main() {
+    Queue q(5);
+
+    q.push(13);
+    q.push(23);
+    q.push(33);
+    q.push(43);
+
+    cout << "The front element is : " << q.getFront() << endl;
+
+}
