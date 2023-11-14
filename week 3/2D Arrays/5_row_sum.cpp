a5b5cb2b656c27a6c42eeea809b41f7c25f0fccd
VishnuVishwas <gvishnuvishwas60@gmail.com>
2023-11-14 11:15:05 +0530

    Row sum

---
+++ week 3/2D Arrays/5_row_sum.cpp
@@ -0,0 +1,27 @@
+// sum of elements in each row
+
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;
+
+int main() {
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for(int i=0; i<3; i++) {
+        int sum = 0;
+        for(int j=0; j<3; j++) {
+            sum = sum + arr[i][j];
+        }  
+        cout << "Row sum is : " << sum << endl;
+    }
+
+    // // column sum 
+    // for(int i=0; i<3; i++) {
+    //     int sum = 0;
+    //     for(int j=0; j<3; j++) {
+    //         sum = sum + arr[j][i];
+    //     }  
+    //     cout << "Row sum is : " << sum << endl;
+    // }
+}
